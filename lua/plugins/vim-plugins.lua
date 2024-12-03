c028425633b6d576414f8a9551404a999ac097b5
Nick Karpenske <randland@gmail.com>
2024-12-02 20:56:23 -0700

    Add some plugins

--- lua/plugins/vim-plugins.lua
+++ lua/plugins/vim-plugins.lua
@@ -1,5 +1,10 @@
 return {
   'rust-lang/rust.vim',
   'jgdavey/vim-blockle',
-  'christoomey/vim-sort-motion'
+  'christoomey/vim-sort-motion',
+  'christoomey/vim-tmux-runner',
+  'tpope/vim-surround',
+  'tpope/vim-repeat',
+  'kana/vim-textobj-user',
+  'kana/vim-textobj-entire'
 }
